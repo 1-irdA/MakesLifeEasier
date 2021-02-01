ff4791ed9adbf3ab96b085c6d420bfbbb62c4b27
Adrien <garrousteadrien@gmail.com>
2021-02-01 01:21:29 +0100

    Update

--- AnalyseRemoteCode/AnalyseRemoteFile.ps1
+++ AnalyseRemoteCode/AnalyseRemoteFile.ps1
@@ -1,11 +1,13 @@
 # Request
-$HTTP_Request = [System.Net.WebRequest]::Create($($args[0]))
+$Http_Request = [System.Net.WebRequest]::Create($($args[0]))
+
 # Response
-$Http_Response = $HTTP_Request.GetResponse()
+$Http_Response = $Http_Request.GetResponse()
+
 # HTTP code
 $Http_Status = [int]$HTTP_Response.StatusCode
 
-If ($Http_Status -eq 200) {
+if ($Http_Status -eq 200) {
 
     $Content = (Invoke-WebRequest -URI $args[0]).Content
     $Http_Response.Close()
